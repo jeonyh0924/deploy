9f1e0b6aada95af9d3fd238323825fa0172414fc
Jeon Young Hoon <hungyb0924@gmail.com>
2018-11-06 11:12:48 +0900

    StaticStorage 구현 및 default_acl사용

--- app/config/settings/dev.py
+++ app/config/settings/dev.py
@@ -11,6 +11,14 @@ DATABASES = secrets['DATABASES']
 
 # django-storages
 DEFAULT_FILE_STORAGE = 'config.storages.MediaStorage'
+#  collectstatic 을 실행했을 떄
+#  버킷의 'static' 폴더 아래에 정적 파일들이 저장되도록 설정해보기
+#  config.starages.StaticStorage 클래스를 만들어서 적용
+
+# STATICFILES_STORAGE = 'config.storages.StaticStorage'
+# 위 설정시 S3 프리티어의 기본 PUT 한계를 금방 초과하게 되므로
+# STATIC_ROOT에 collectstatic 후 Nginx에서 제공하는 형태로 사용
+
 AWS_ACCESS_KEY_ID = secrets['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = secrets['AWS_SECRET_ACCESS_KEY']
 AWS_STORAGE_BUCKET_NAME = secrets['AWS_STORAGE_BUCKET_NAME']
